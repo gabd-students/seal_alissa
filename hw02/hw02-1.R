150482cafcceae256ddd40808560b7cb37cb52bc
Alissa Seal <adbuchanan1s@semo.edu>
2020-07-01 11:57:47 -0500

    Section 1.1 - 1.4 Commmit

---
+++ hw02/hw02-1.R
@@ -0,0 +1,65 @@
+# Alissa Seal
+
+# HW02 Part 1: Intro to Basics
+
+### Section 1.1  Comments and calculators
+
+# Calculate 30 + 12
+
+30 + 12
+
+### Section 1.2 Arithmetic with R
+
+# Division 121/1.1
+
+121/1.1
+
+#Exponentiation 2^5
+
+2^5
+
+#Modulo
+
+28%%6
+
+#Exponentiation without parentheses
+
+49^1/2
+
+#Exponentation with parentheses
+
+49^(1/2)
+
+### Section 1.3 Variable Assignment
+
+# Assign the value 42 to x
+
+x <- 42 
+
+# Print out the value of the variable x
+
+x
+
+#Assign the value of 24 to the variable d_merriami
+
+d_merriami <-24
+
+#Assign the value 9 to d_ordii
+
+d_ordii <-9
+
+# Check the results
+
+### Section 1.4 Working with variables
+
+# Correcting the mistake
+
+d_merriami + 5
+
+d_merriami 
+
+# Correcting the mistake
+
+d_merriami <- d_merriami + 5
+d_merriami
+
