e394ea0b751a594cfbf917ff042e5746dc260d08
Alissa Seal <adbuchanan1s@semo.edu>
2020-07-02 14:37:17 -0500

    finish 2.7

--- hw02/hw02-2.R
+++ hw02/hw02-2.R
@@ -24,7 +24,7 @@ cultures_without_pplo <- c(4.6, 4.8, 5.1, 5.5, 5.8)
 
 ## Cultures with pplo contaminant
 
-cultures_with_pplo <- c(4.6, 4.7, 4.8, 4.9 4.8)
+cultures_with_pplo <- c(4.6, 4.7, 4.8, 4.9, 4.8)
 
 # Section 2.4: Name your vectors
 
@@ -34,37 +34,49 @@ days_sampled <- c('Day 0', 'Day2', 'Day 4', 'Day 6', 'Day 8')
 
 ## Name your two data vectors with the 'days_sampled' vector
 
-names (days_sampled) <- c('cultures_without_pplo', 'cultures_with_pplo')
+names(cultures_without_pplo) <- days_sampled
+
+## Name your two data vectors with the 'days_sampled' vector
+
+names(cultures_with_pplo) <- days_sampled
 
 ## Check that two data vectors were correctly named
 
 days_sampled 
 
+# Section 2.5: Calculations with vectors
+
 ## Find the maximum values in cultures_without_pplo
 
-max (cultures_without_pplo)
+max(cultures_without_pplo)
 
 ## Store max value for max_without_pplo
 
-max_without_pplo <-5.8
+max_without_pplo <- max(cultures_without_pplo)
 
-## Find the maximum value in cultures_with_ppl
+## Find the maximum value in cultures_with_pplo
 
-max (cultures_with_pplo)
+max(cultures_with_pplo)
 
 ## Store max value for max_with_pplo
 
-max_with_pplo <-4.9
+max_with_pplo <- max(cultures_with_pplo)
 
 ## Find the minimum values in cultures_without_pplo
 
-min (cultures_without_pplo)
+min(cultures_without_pplo)
 
 ## Store value for min_without_pplo
 
-min_without_pplo <-4.6
+min_without_pplo <- min(cultures_without_pplo)
 
-# Section 2.5: Calculations with vectors
+## Find the minimum values in cultures_with_pplo
+
+min(cultures_with_pplo)
+
+## Store the value for min_with_pplo
+
+min_with_pplo <- min(cultures_with_pplo)
 
 ## Calculate the actual number of cells without pplo
 
@@ -72,11 +84,23 @@ min_without_pplo <-4.6
 
 ## Store in cell_counts_without_pplo
 
-cell_counts_without_pplo <- c(39810.72, 63095.73, 125892.54, 316227.77, 630957.34)
+cell_counts_without_pplo <- c(10^cultures_without_pplo)
 
-## Calculate the average for cell_counts_without_pplo
+## Calculate the average for cells with pplo
+
+10^cultures_with_pplo
+
+## store in cell_counts_with_pplo
+
+cell_counts_with_pplo <- c(10^cultures_with_pplo)
+
+## calculate the average for cell_counts_without_pplo
 
 mean(cell_counts_without_pplo)
+
+## calcutlate the average for cell_counts_with_pplo
+
+mean(cell_counts_with_pplo)
 
 # Section 2.6: Extract individual elements from a vector
 
@@ -96,9 +120,9 @@ cultures_with_pplo[c('Day 2', 'Day 4')]
 
 ## Use cell_counts_without_pplo to create a logical vector for cell counts greater than 100000
 
-cell_counts_without_pplo>1000000
+cell_counts_without_pplo_greater_than_100000 <- c(cell_counts_without_pplo>1000000)
 
-## Use that vector to show the actual cell counts from culture_without_pplo
+## Use that vector to show the days from culture_without_pplo greater than 100000
 
-
+cell_counts_without_pplo_greater_than_100000
   
