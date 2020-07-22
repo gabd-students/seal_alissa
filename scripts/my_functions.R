a999f77cbfc1cb01b0bbff90d8f81418503296ae
Alissa Seal <adbuchanan1s@semo.edu>
2020-07-22 15:27:15 -0500

    Hopefully, it is right now!!!

--- scripts/my_functions.R
+++ scripts/my_functions.R
@@ -2,10 +2,14 @@
 ## Calculating the standard error of mean
 ## The first argument tells whether to remove NA, defaults to FALSE
 
-std_err <- function(x, na.rm= FALSE) {sqrt(var(x)/length(x))}
+std_error <- function(values,na.rm=FALSE){   
+  ifelse(na.rm,                                 # Using ifelse() because we have 2 values
+         values <- na.omit(values),             # If NA is TRUE
+         values)                                # If NA is FALSE
+  (sqrt(var(values)/length(values)))            # Standard error formula
+}
 
-
-### Scaled Mass Index Function????
+### Scaled Mass Index Function
 
 scaled_mass <- function(mass = 0, tarus = 0, slope =0) {mass*((mean(tarsus))/tarsus)^slope}
 
