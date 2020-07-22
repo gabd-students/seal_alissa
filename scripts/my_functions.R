20153539c981abd490bc08dd9176e217544974ff
Alissa Seal <adbuchanan1s@semo.edu>
2020-07-22 11:57:00 -0500

    Corrections

--- scripts/my_functions.R
+++ scripts/my_functions.R
@@ -7,18 +7,19 @@ std_err <- function(x, na.rm= FALSE) {sqrt(var(x)/length(x))}
 
 ### Scaled Mass Index Function????
 
-scaled_mass <- function(mass = 0, tarus = 0, slope =0) {mass*((mean(tarus))/tarus)^slope}
+scaled_mass <- function(mass = 0, tarus = 0, slope =0) {mass*((mean(tarsus))/tarsus)^slope}
 
 ### Mass Vector
 
 mass <- c(26.7, 22.8, 25.7, 26.1, 23.9)
 
-### Tarus Vector
+### Tarsus Vector
 
-tarus <- c(18.4, 17.5, 18.4, 18.0, 18.2)
+tarsus <- c(18.4, 17.5, 18.4, 18.0, 18.2)
 
 ### Assign Slope
 
 slope <- 1.5
+
 
 
